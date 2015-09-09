cb6582e915566fa3c104786bd592232c1f776095
Vilas <vilash.9116@gmail.com>
2015-09-09 15:57:09 +0530

    Add dummy for Openshift

---
+++ public/dummy.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bus Go</title>
+		<link rel="stylesheet" type="text/css" href="css/public.css">
+	</head>
+
+	<body>
+
+
+		<div id="header">
+			<h1>Bus Go</h1> 
+		</div>
+
+	</body>
+</html>
